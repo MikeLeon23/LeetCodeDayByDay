cd45fae8d8be4a637c0e3ae6876d5a13a11e0708
MikeLeon23 <793766131@qq.com>
2020-04-20 20:05:16 +0800

    nothing special

--- 680.验证回文字符串-ⅱ.js
+++ 680.验证回文字符串-ⅱ.js
@@ -45,71 +45,71 @@
  */
 
 // 解法一：双指针法
-// var validPalindrome = function (s) {
-//     if (isReverseStr(s)) {
-//         return true;
-//     }
+var validPalindrome = function (s) {
+    if (isReverseStr(s)) {
+        return true;
+    }
 
-//     for(var i = 0, j = s.length-1; i < j; i++, j--){
-//         if(s.charAt(i) != s.charAt(j)) {
-//             break;
-//         }
-//     }
+    for(var i = 0, j = s.length-1; i < j; i++, j--){
+        if(s.charAt(i) != s.charAt(j)) {
+            break;
+        }
+    }
 
-//     var arr = s.split('')
-//     var arrCopy = s.split('');
-//     arr.splice(i, 1);
-//     if (isReverseStr(arr.join(''))) {
-//         return true;
-//     }
-//     arrCopy.splice(j, 1);
-//     if (isReverseStr(arrCopy.join(''))) {
-//         return true;
-//     }
+    var arr = s.split('')
+    var arrCopy = s.split('');
+    arr.splice(i, 1);
+    if (isReverseStr(arr.join(''))) {
+        return true;
+    }
+    arrCopy.splice(j, 1);
+    if (isReverseStr(arrCopy.join(''))) {
+        return true;
+    }
 
-//     return false;
+    return false;
 
-// };
+};
 
-// // 判断是否是回文字符串的函数
-// function isReverseStr(s) {
-//     var i = 0;
-//     var j = s.length - 1;
-//     while (i < j) {
-//         if (s.charAt(i) == s.charAt(j)) {
-//             i++;
-//             j--;
-//         } else {
-//             return false;
-//         }
-//     }
-//     return true;
-// }
+// 判断是否是回文字符串的函数
+function isReverseStr(s) {
+    var i = 0;
+    var j = s.length - 1;
+    while (i < j) {
+        if (s.charAt(i) == s.charAt(j)) {
+            i++;
+            j--;
+        } else {
+            return false;
+        }
+    }
+    return true;
+}
 
 
-// // 解法二：双指针法重写
-// var validPalindrome = function (s) {
-//     for(var i = 0, j = s.length-1; i < j; i++, j--){
-//         if(s.charAt(i) != s.charAt(j)){
-//             return isReverseStr(s, i+1, j) || isReverseStr(s, i, j-1)
-//         }
-//     }
+// 解法二：双指针法重写
+var validPalindrome = function (s) {
+    for(var i = 0, j = s.length-1; i < j; i++, j--){
+        if(s.charAt(i) != s.charAt(j)){
+            return isReverseStr(s, i+1, j) || isReverseStr(s, i, j-1)
+        }
+    }
 
-//     return true;
-// };
+    return true;
+};
 
-// // 判断回文字符串函数重写
-// var isReverseStr = function(s, i, j) {
-//     while(i < j){
-//         if(s.charAt(i) != s.charAt(j)){
-//             return false;
-//         }else{
-//             i++;
-//             j--;
-//         }
-//     }
-//     return true;
-// }
+// 判断回文字符串函数重写
+var isReverseStr = function(s, i, j) {
+    while(i < j){
+        if(s.charAt(i) != s.charAt(j)){
+            return false;
+        }else{
+            i++;
+            j--;
+        }
+    }
+    return true;
+}
 
 // 解法三：双指针法加递归
 var validPalindrome = function (s, flag = true) {   // flag用于控制递归深度，只需要递归一次
