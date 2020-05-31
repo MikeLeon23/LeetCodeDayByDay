4ca9963e88504c82600bf32c468a1bacd04eb228
MikeLeon23 <793766131@qq.com>
2020-05-31 21:39:54 +0800

    刷题

---
+++ 141.环形链表.js
@@ -0,0 +1,100 @@
+/*
+ * @lc app=leetcode.cn id=141 lang=javascript
+ *
+ * [141] 环形链表
+ *
+ * https://leetcode-cn.com/problems/linked-list-cycle/description/
+ *
+ * algorithms
+ * Easy (47.26%)
+ * Likes:    619
+ * Dislikes: 0
+ * Total Accepted:    163.5K
+ * Total Submissions: 340.2K
+ * Testcase Example:  '[3,2,0,-4]\n1'
+ *
+ * 给定一个链表，判断链表中是否有环。
+ * 
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ * 
+ * 
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 输入：head = [1,2], pos = 0
+ * 输出：true
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ * 
+ * 
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 输入：head = [1], pos = -1
+ * 输出：false
+ * 解释：链表中没有环。
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 进阶：
+ * 
+ * 你能用 O(1)（即，常量）内存解决此问题吗？
+ * 
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {boolean}
+ */
+
+// 方法一：双指针，一个指针每次移动一个节点，一个指针每次移动两个节点，如果存在环，那么这两个指针一定会相遇。
+var hasCycle = function (head) {
+    if (head === null) {
+        return false;
+    }
+    let l1 = head, l2 = head.next;
+    while (l1 != null && l2 != null && l2.next != null) {
+        if (l1 === l2) {
+            return true;
+        }
+        l1 = l1.next;
+        l2 = l2.next.next;
+    }
+    return false;
+};
+
+// 方法二：哈希表（借助map）
+var hasCycle = function (head) {
+    var map = new Map();
+    while(head){
+        if(map.has(head)){
+            return true;
+        }
+        map.set(head, 1);
+        head = head.next;
+    }
+    return false;
+};
+// @lc code=end
+
