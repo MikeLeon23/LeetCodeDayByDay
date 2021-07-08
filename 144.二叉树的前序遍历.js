c8e3505546e2595877639ae7788c5c7fe0305b78
liangzhihao <17665356935@qq.com>
2021-07-08 18:11:16 +0800

    没按照题目要求用迭代的方式解答

---
+++ 144.二叉树的前序遍历.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=144 lang=javascript
+ *
+ * [144] 二叉树的前序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function(root) {
+  const result = []
+  ;(function(root) {
+    if (root === null) {
+      return
+    }
+    result.push(root.val)
+    arguments.callee(root.left)
+    arguments.callee(root.right)
+  })(root)
+  return result
+};
+// @lc code=end
+
